29dee925a6e2d72be0d519366d8761dd72fcf24e
Ben Crowder <ben.crowder@gmail.com>
2010-11-08 20:47:52 -0700

    Initial commit

---
+++ alibaba_config.sample.php
@@ -0,0 +1,20 @@
+<?
+// Alibaba config file
+//
+// Included by Alibaba.class.php
+
+$APP_NAME = "yourapp";
+
+$DATABASE_NAME = "mydatabase";
+$DATABASE_HOST = "localhost";
+$DATABASE_USERNAME = "";
+$DATABASE_PASSWORD = "";
+$USER_TABLE_NAME = "users";
+$USERNAME_FIELD = "username";
+$PASSWORD_FIELD = "password";
+
+$COOKIE_EXPIRATION = "30";			// in days
+
+$HASH_FUNCTION = "md5";				// or "sha1" or "md5sha1" or "sha1md5"
+
+$LOGIN_PAGE_URL = "http://yourdomain.com/app/login.php";
